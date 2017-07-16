301d382010ef6a2478eab836b023a18646307117
pranitsawantAtBwellthy <pranit.sawant@wellthy.care>
2017-07-16 15:35:28 +0530

    added functions package

--- src/app.kt
+++ src/app.kt
@@ -1,4 +1,5 @@
 import classesandinheritance.Person
+import functionsandlambas.Foo
 
 /**
  * Created by pranit on 16/7/17.
@@ -7,4 +8,6 @@ fun main(args: Array<String>){
     println("Hello World!")
 
     var person = Person("pranit", 28)
+    var d = Foo().double(10)
+    println(d   )
 }
---
+++ src/functionsandlambas/Foo.kt
@@ -0,0 +1,9 @@
+package functionsandlambas
+
+/**
+ * Created by pranit on 16/7/17.
+ */
+class Foo {
+
+    fun double(x: Int) : Int = 2 * x
+}
